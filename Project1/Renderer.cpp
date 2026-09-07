8ff108d35d7130249b313357a1ad8b681bc38739
유채영 <chaeyeong.yu0819@gmail.com>
2026-09-07 23:52:29 +0900

    셰이더 오류 수정

--- Project1/Renderer.cpp
+++ Project1/Renderer.cpp
@@ -172,7 +172,7 @@ bool Renderer::CreateVertexShader(LPCWSTR path, LPCSTR entryPoint, ID3D11VertexS
 
 	hr = Device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, outVS);
 
-	if (!outBlob)
+	if (outBlob)
 	{
 		*outBlob = vsBlob;
 	}
